3cc6e1a4639537003c5a272a3ad548a7822beaae
marc_bayart <marc_bayart@ens.univ-artois.fr>
2021-03-30 15:06:51 +0200

    Games service updated

--- src/app/_services/games.service.ts
+++ src/app/_services/games.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {environment} from '../../environments/environment';
-import {catchError, map} from 'rxjs/operators';
+import {catchError, find, map} from 'rxjs/operators';
+import {Jeu} from '../_models/jeu';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -17,6 +18,14 @@ export class GamesService {
 
   list(): Observable<any> {
     return this.http.get<any>(`${environment.apiUrl}/jeux`, httpOptions)
+      .pipe(
+        map(g => g.data.item),
+        catchError(err => throwError(err))
+      );
+  }
+
+  getGameById(id: number): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}/jeux/${id}`, httpOptions)
       .pipe(
         map(g => g.data.item),
         catchError(err => throwError(err))
