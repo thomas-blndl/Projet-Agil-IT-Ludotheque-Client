63a8555d96b748ac3608664462494e5837dcfd4b
marc_bayart <marc_bayart@ens.univ-artois.fr>
2021-03-30 16:37:29 +0200

    Game details added

--- src/app/game-detail/game-detail.component.ts
+++ src/app/game-detail/game-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {GamesService} from '../_services/games.service';
-import {filter, find} from 'rxjs/operators';
 
 @Component({
   selector: 'app-game-detail',
@@ -16,7 +15,12 @@ export class GameDetailComponent implements OnInit {
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.game = this.gamesService.getGameById(id);
+    // let div = document.createElement('div');
+    this.gamesService.getGameById(id).subscribe((rep) => {
+      this.game = rep;
+      /*div.innerHTML = this.game.regles;
+      this.game.regles = div.innerText;*/
+    });
   }
 
 }
