e2779255260addd1689a2f77227e89e321742627
Degardin Julien <julien_degardin@ens.univ-artois.fr>
2021-03-31 16:00:50 +0200

    Issue17

--- src/app/game-detail/game-detail.component.ts
+++ src/app/game-detail/game-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {GamesService} from '../_services/games.service';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-game-detail',
@@ -10,7 +12,7 @@ import {GamesService} from '../_services/games.service';
 export class GameDetailComponent implements OnInit {
 
   game;
-
+  dateS: string;
   constructor(private route: ActivatedRoute, private gamesService: GamesService) { }
 
   ngOnInit(): void {
@@ -21,6 +23,11 @@ export class GameDetailComponent implements OnInit {
       /*div.innerHTML = this.game.regles;
       this.game.regles = div.innerText;*/
     });
+  }
+
+  timeFrom(myDate: Date): string {
+    this.dateS = moment(myDate).fromNow();
+    return this.dateS;
   }
 
 }
